da0500c402998ea58e0b14f6aadd7e864f8ddc07
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2021-01-18 19:54:04 +0200

    Add unit test TestCest

---
+++ tests/unit/Folder/SubFolder/TestCest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Folder\SubFolder;
+
+use UnitTester;
+
+class TestCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+    }
+
+    public function InterestingTest(UnitTester $I)
+    {
+    }
+}
